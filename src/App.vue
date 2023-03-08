4a647e73d37d70168f9701f778eb507b68b486d9
forest079 <bluegx08@gmail.com>
2023-03-08 16:47:57 +0800

    fix 4

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Home3</RouterLink>
+        <RouterLink to="/">Home4</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
